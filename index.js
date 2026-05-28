e9b28ba4529d33a402598b35b2e5f60eb12d9fa8
WalToledo <toledowalter836@gmail.com>
2026-05-28 10:21:02 -0300

    Contador

---
+++ index.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById("btnSumar");
+var txt = document.getElementById("txtAcum");
+
+var counter = 0;
+
+btn.addEventListener("click", function(){
+    counter = counter+1;
+    txt.innerText = `Cuenta: ${counter}`; //fran crack dice que con el `` se formatea usando ${}
+}) 
